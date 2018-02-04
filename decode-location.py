eb62dc1289092f210fff6783acd277eedbfb826e
ꓛ㇜ay <cwells@cwells.org>
2018-02-04 15:45:53 -0500

    Updates location, add the encoding

--- decode-location.py
+++ decode-location.py
@@ -2,7 +2,7 @@
 import binascii as bin
 import base64
 
-location='4d6a457749464e766458526f49444d7a636d51675531517349464e4c534445784e413d3d'
+location='add encoded text here'
 
 # location looks like hex so lets unhexlify it
 l = bin.unhexlify(location)
